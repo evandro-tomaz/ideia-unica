e522eda9f823dbf335ec67746bcb74458f9c0c3a
Evandro Tomaz <tmzplaydev@gmail.com>
2023-09-06 23:56:17 -0300

    Nova Home

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,7 @@ function Home() {
     return (
         <div>
             <h1>Home Page</h1>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador() {
-    const [cont, setCont] = useState(1);
-
-    function addCont() {
-        setCont(cont + 1);
-    }
-
-    return (
-        <div>
-            <h1>{cont}</h1><br />
-            <button onClick={addCont}>Aumentar contador</button>
+            <p>Em construção</p>
         </div>
     )
 }
