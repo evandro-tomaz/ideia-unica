dbdc1e9fa12c7c355961daaa920b9e185f2e22d5
Evandro Tomaz <tmzplaydev@gmail.com>
2023-09-07 00:47:54 -0300

    Teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import Link from "next/link";
 
 function Home() {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Home</h1>
             <p>Em construção</p>
+            <Link href="/sobre">
+                <a>Acessar página sobre</a>
+            </Link>
         </div>
     )
 }
---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+function Sobre() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <p>Em construção</p>
+            <Link href="/">
+                <a>Acessar página home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sobre
