df59e4d247fa78e5c88c8187174aa14b7a02a1cd
Evandro Tomaz <tmzplaydev@gmail.com>
2023-09-20 13:46:32 -0300

    Some fixes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function HomePage() {
     return (
         <div>
-            <h1>Home Evoware</h1>
+            <h1>Home - Evoware</h1>
             <h2>Bem-vindo a Evoware</h2>
             <p>Em construção...</p>
         </div>
